6767ad56dd44a96bdec47d7612fd2ae0af1dad46
lawrencefej <lawrencefej@gmail.com>
2018-09-01 15:28:41 -0400

    Day 2 complete

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -10,6 +10,23 @@ namespace Day2
     {
         static void Main(string[] args)
         {
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            Solve(meal_cost, tip_percent, tax_percent);
+
+            Console.ReadLine();
+        }
+
+        static void Solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            double tip = ((meal_cost * tip_percent)/100);
+            double tax = ((meal_cost * tax_percent) / 100);
+            var totalCost = meal_cost + tax + tip;
+            Console.WriteLine("The total meal cost is {0} dollars.", Math.Round(totalCost) );
         }
     }
 }
